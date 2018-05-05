35c0329d7954a1e75c1dce407bf7717b17b8c939
Anwesh43 <anweshthecool0@gmail.com>
2018-05-05 19:06:37 +0530

    adding ColorBarFillStage

---
+++ ColorBarFillStage.ts
@@ -0,0 +1,29 @@
+const w : number = window.innerWidth, h :number = window.innerHeight
+class ColorBarFillStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+
+    context : CanvasRenderingContext2D
+
+    constructor() {
+        this.initCanvas()
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
